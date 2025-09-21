08aeb36ba9f1a52532dfbd22d3b5346c5f13865f
zhaley720 <thez.haley@yahoo.com>
2025-09-21 13:25:25 -0400

    finished 1 hour of coding

--- javascript-programming-course/5-chrome-extension/23. Add the <a> tag/index.js
+++ javascript-programming-course/5-chrome-extension/23. Add the <a> tag/index.js
@@ -14,7 +14,7 @@ function renderLeads() {
     for (let i = 0; i < myLeads.length; i++) {
         // Wrap the lead in an anchor tag (<a>) inside the <li>
         // Can you make the link open in a new tab?
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += "<li><a target='_blank' href='" +myLeads[i] + "'>" + myLeads[i] + "</a></li>"
     }
     ulEl.innerHTML = listItems  
 }
--- javascript-programming-course/5-chrome-extension/24. Template strings/index.js
+++ javascript-programming-course/5-chrome-extension/24. Template strings/index.js
@@ -13,6 +13,10 @@ function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+    listItems += `
+        <li>
+            <a target="_blank" href="${myLeads[i]}">${myLeads[i]}</a>
+        </li>`
     }
     ulEl.innerHTML = listItems  
 }
