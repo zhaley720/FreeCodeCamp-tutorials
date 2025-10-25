03d2aa317401c480543c248d18a11d8cdfba4ff2
zhaley720 <thez.haley@yahoo.com>
2025-10-25 03:24:24 -0400

    saving progress

---
+++ data-structures-and-algorithms-megacourse/05.group-anagrams--medium-#49/java/GroupAnagrams.java
@@ -0,0 +1,32 @@
+package java;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ansMap = new HashMap<>();
+        int[] count = new int[26];
+        for (String s:strs) {
+            Arrays.fill(count, 0);
+            for (char c:s.toCharArray()) {
+                count[c-'a']++;
+            }
+            StringBuilder sb = new StringBuilder("");
+            for (int i = 0; i < 26; i++) {
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key = sb.toString();
+            if (!ansMap.containsKey(key)) {
+                ansMap.put(key, new ArrayList());
+            }
+            ansMap.get(key).add(s);
+        }
+        return new ArrayList(ansMap.values());
+    }
+}
